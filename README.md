b5f9669a2bc443ff0fa7def7add02a48c7cc5357
Alex Farr <me@alexfarr.co.uk>
2022-05-19 01:05:11 +0100

    update

--- README.md
+++ README.md
@@ -2,5 +2,11 @@
 
 # resources
 https://www.youtube.com/watch?v=oCMOYS71NIU
+
 https://lpnplant.io/web-bluetooth/
+
 https://web.dev/bluetooth/
+
+https://googlechrome.github.io/samples/web-bluetooth/notifications.html?service=alert_notification
+
+https://github.com/nkolban/ESP32_BLE_Arduino
--- docroot/js/app.js
+++ docroot/js/app.js
@@ -1,6 +1,58 @@
+let findBtn = document.getElementById("ble-find");
+let page = document.getElementsByClassName('page')[0];
+let element = document.getElementsByClassName("element");
 
-var findBtn = document.getElementById("ble-find");
+findBtn.addEventListener('click', e => {
+    navigator.bluetooth.requestDevice({'acceptAllDevices': true})
+        .then(device => {
+            // Human-readable name of the device.
+            console.log(device.name);
 
-findBtn.addEventListener('click', function(event){
-    navigator.bluetooth.requestDevice({'acceptAllDevices':true})
+            // Attempts to connect to remote GATT Server.
+            return device.gatt.connect();
+        })
+        .then(service => {
+            // Getting Battery Level Characteristic…
+            return service.getCharacteristic('serial');
+        })
+        .then(characteristic => {
+            // Reading Battery Level…
+            return characteristic.readValue();
+        })
 });
+
+let mouseMovePrevVal = 0;
+page.addEventListener('mousemove', e => {
+    let value = 0;
+    if (e.offsetY > mouseMovePrevVal) {
+        value = 1;
+    } else {
+        value = -1;
+    }
+    bluIncrementAction(value, thing);
+    mouseMovePrevVal = e.offsetY;
+});
+
+/**
+ *
+ * @param {string} direction
+ * @param {object} thingObject
+ */
+let bluIncrementAction = (direction, thingObject) => {
+    if (direction == 1) {
+        thingObject.value = thingObject.value + thingObject.step;
+    } else if (direction == -1 && thingObject.value > 0) {
+        thingObject.value = thingObject.value - thingObject.step;
+    }
+    thingObject.callback(thingObject);
+};
+
+
+let thing = {
+    value: 0,
+    step: 10,
+    selector: "element",
+    callback: (thingObject) => {
+        document.getElementsByClassName(thingObject.selector)[0].style.height = thingObject.value + "px";
+    },
+};
